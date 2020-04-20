97e69db43233480ef3a137cb81de956e033d23bd
igor <igor.piddubnyi@gmail.com>
2020-04-20 16:52:27 +0200

    Implement websocket-ssl config

---
+++ readme.md
@@ -0,0 +1,10 @@
+Tested with self-signed certificate
+
+generate sertificate:
+keytool -genkeypair -keyalg RSA -keystore self_signed_localhost.jks -alias self_signed_localhost
+
+enter the password, I used '123456', other fields could be left blank
+
+run Main, to start the route
+
+run WebsocketTest to check websocket.  
--- src/test/java/test/WebsocketTest.java
+++ src/test/java/test/WebsocketTest.java
@@ -3,54 +3,20 @@ package test;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClient;
 import org.asynchttpclient.ws.WebSocket;
-import org.asynchttpclient.ws.WebSocketListener;
 import org.asynchttpclient.ws.WebSocketUpgradeHandler;
 import org.junit.Test;
+
+import static org.asynchttpclient.DefaultAsyncHttpClientConfig.Builder;
 
 public class WebsocketTest {
 
     @Test
     public void testWSHttpCall() throws Exception {
-        AsyncHttpClient client = new DefaultAsyncHttpClient();
-
-        WebSocket websocket = client.prepareGet("ws://127.0.0.1:8181/test").execute(
-                new WebSocketUpgradeHandler.Builder()
-                        .addWebSocketListener(new WebSocketListener() {
-                            @Override
-                            public void onOpen(WebSocket websocket) {
-                            }
-
-                            @Override
-                            public void onClose(WebSocket websocket, int code, String reason) {
-
-                            }
-
-                            @Override
-                            public void onError(Throwable t) {
-                                t.printStackTrace();
-                            }
-
-                            @Override
-                            public void onBinaryFrame(byte[] payload, boolean finalFragment, int rsv) {
-                                System.out.println(new String(payload));
-                            }
-
-                            @Override
-                            public void onTextFrame(String payload, boolean finalFragment, int rsv) {
-                                System.out.println(payload);
-                            }
-
-                            @Override
-                            public void onPingFrame(byte[] payload) {
-
-                            }
-
-                            @Override
-                            public void onPongFrame(byte[] payload) {
-
-                            }
-                        }).build()).get();
-
+        AsyncHttpClient client = new DefaultAsyncHttpClient(
+                new Builder().setUseInsecureTrustManager(true).build()
+        );
+        WebSocket websocket = client.prepareGet("wss://127.0.0.1:8181/test")
+                .execute(new WebSocketUpgradeHandler.Builder().build()).get();
         websocket.sendTextFrame("Data");
         websocket.sendCloseFrame();
         client.close();
